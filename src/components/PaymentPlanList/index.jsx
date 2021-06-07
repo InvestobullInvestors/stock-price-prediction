6548bad190e8991ed69b8966bf967ebbeac3e3ef
Himanshu Goyal <hgoyal@dhcp-206-87-197-133.ubcsecure.wireless.ubc.ca>
2021-06-06 19:42:58 -0700

    Himanshu/Plans setup modal

--- src/components/PaymentPlanList/index.jsx
+++ src/components/PaymentPlanList/index.jsx
@@ -4,13 +4,13 @@ import PaymentPlanCard from '../PaymentPlanCard'
 
 const PaymentPlanList = (props) => (
     <SimpleGrid columns={{base: 1, md: 3}} spacing={5}>
-        <PaymentPlanCard type="Free" price="0" buttonText="Start Exploring"
+        <PaymentPlanCard type="Free" price={0} buttonText="Start Exploring"
                          details={["This plan is the best in the world",
                              " This plan does nothing"]}/>
-        <PaymentPlanCard type="Free" price="0" buttonText="Start Exploring"
+        <PaymentPlanCard type="Premium" price={5} buttonText="Subscribe Now"
                          details={["This plan is the best in the world",
                              " This plan does nothing"]}/>
-        <PaymentPlanCard type="Free" price="0" buttonText="Start Exploring"
+        <PaymentPlanCard type="Unlimited" price={10} buttonText="Subscribe Now"
                          details={["This plan is the best in the world",
                              " This plan does nothing"]}/>
     </SimpleGrid>
