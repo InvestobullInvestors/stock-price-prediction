c1464df34a36b020b9b90e9ca9789a19cc5e5b09
Weiyoung Tan <wtan@bgcengineering.ca>
2021-06-23 16:39:50 -0700

    refactor, restyle

--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -3,14 +3,7 @@ import {Button, Heading, Center, Flex, Spacer} from "@chakra-ui/react";
 import PageTemplate from "../../components/PageTemplate";
 import NewsGrid from "../../components/NewsGrid";
 import NewsSourceDrawer from "../../components/NewsSourceDrawer";
-
-const CustomHeading = ({children, ...otherProps}) => (
-    <Center>
-        <Heading as="h2" size="xl" {...otherProps}>
-            {children}
-        </Heading>
-    </Center>
-)
+import CustomHeading from "../../components/CustomHeading";
 
 const News = () => {
     const [color, setColor] = useState("green");
