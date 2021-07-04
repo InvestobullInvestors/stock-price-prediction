455f745d35aa2b7e9612463ada6e17811a6e1ccc
Himanshu Goyal <hgoyal@dhcp-206-87-192-103.ubcsecure.wireless.ubc.ca>
2021-07-04 14:06:10 -0700

    payment success and failure alerts

--- src/contexts/useHandlePayment.js
+++ src/contexts/useHandlePayment.js
@@ -1,15 +1,20 @@
 import {CardElement} from "@stripe/react-stripe-js";
 import {chargeCard} from "../services/PaymentService";
 
-const useHandlePayment = (payableAmount, stripe, elements) => {
+const useHandlePayment = (payableAmount, stripe, elements, callback) => {
     return (async (event) => {
         event.preventDefault();
-        const {error, paymentMethod: {id}} = await stripe.createPaymentMethod({
-            type: "card",
-            card: elements.getElement(CardElement)
-        })
-        if (!error) {
-            await chargeCard(id, payableAmount);
+        try {
+            const {error, paymentMethod: {id}} = await stripe.createPaymentMethod({
+                type: "card",
+                card: elements.getElement(CardElement)
+            })
+            if (!error) {
+                const status = await chargeCard(id, payableAmount);
+                callback(status)
+            }
+        } catch (error) {
+            console.log(error.message)
         }
     })
 }
