826b4060ff247a271c093a8854909ab5689f1751
Himanshu Goyal <hgoyal@dhcp-206-87-192-103.ubcsecure.wireless.ubc.ca>
2021-07-04 00:45:14 -0700

    version related changes

--- server/dal/stock-info.js
+++ server/dal/stock-info.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require('dotenv');
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING)
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
 
 var Schema = mongoose.Schema
 
