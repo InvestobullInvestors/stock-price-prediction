175d7dd7412d29e787f0d1199a73b207eb9c8e86
Raghav Thakur <raghav.thakur.rt.20@gmail.com>
2021-07-17 04:01:55 -0700

    Rewrote searchbar + api calls

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,26 @@ var express = require('express');
 var router = express.Router();
 const { realtimeStockInfo } = require('../dal/stock-markets');
 
-const defaultStockList = [{}];
+const defaultStockList = [
+    {
+        company_name: 'Tesla',
+        ticker: 'TSLA',
+        current_price: 615.18,
+        previous_close: 570.15,
+        open: 602.14,
+        last_price: 609.89,
+        change: -0.23,
+        change_percent: '-0.04%',
+        currency: 'USD',
+        volume: '16.205M',
+        pe_ratio: '585',
+        shares_owned: 553,
+        avg_vol: '31.399M',
+        market_cap: '587.525B',
+        fifty_two_week_low: 321.24,
+        fifty_two_week_high: 900.84,
+    },
+];
 
 /* GET home page. */
 // TODO: add response codes
@@ -10,6 +29,7 @@ router.get('/', function (req, res) {
     realtimeStockInfo
         .find()
         .then((stockList) => {
+            console.log('Send Res: ', stockList);
             res.send(stockList);
         })
         .catch((error) => {
