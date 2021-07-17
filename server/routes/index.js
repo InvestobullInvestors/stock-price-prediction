ff5101671cfa0470863aa90f445b0c73d1523646
Raghav Thakur <raghav.thakur.rt.20@gmail.com>
2021-07-17 00:53:33 -0700

    DB connection

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,48 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const { realtimeStockInfo } = require('../dal/stock-markets');
 
-const stockList = [
-    {
-        company_name: 'Tesla',
-        ticker: 'TSLA',
-        current_price: 615.18,
-        previous_close: 570.15,
-        open: 602.14,
-        last_price: 609.89,
-        change: -0.23,
-        change_percent: '-0.04%',
-        currency: 'USD',
-        volume: '16.205M',
-        pe_ratio: '585',
-        shares_owned: 553,
-        avg_vol: '31.399M',
-        market_cap: '587.525B',
-        fifty_two_week_low: 321.24,
-        fifty_two_week_high: 900.84,
-    },
-    {
-        company_name: 'AMC',
-        ticker: 'AMC',
-        current_price: 49.11,
-        last_price: 49.34,
-        previous_close: 49.15,
-        open: 52.14,
-        change: -5.71,
-        change_percent: '-10.37%',
-        currency: 'USD',
-        volume: '138M',
-        pe_ratio: 'N/A',
-        shares_owned: 0,
-        avg_vol: '141M',
-        market_cap: '25.33B',
-        fifty_two_week_low: 2.54,
-        fifty_two_week_high: 72.19,
-    },
-];
+const defaultStockList = [{}];
 
 /* GET home page. */
+// TODO: add response codes
 router.get('/', function (req, res) {
-    res.send(stockList);
+    realtimeStockInfo
+        .find()
+        .then((stockList) => {
+            res.send(stockList);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.send(defaultStockList);
+        });
 });
 
 module.exports = router;
--- src/components/StockSearchBar/index.jsx
+++ src/components/StockSearchBar/index.jsx
@@ -11,51 +11,159 @@ import {
 } from '@chakra-ui/react';
 import { GoSearch } from 'react-icons/go';
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import { AnimatePresence, motion } from 'framer-motion';
+
+// const StockSearchBar = () => {
+//     const boxColor = useColorModeValue('brand.100', 'brand.700');
+//     const [value, setValue] = useState('');
+//     const color = useColorModeValue('blue', 'gray');
+
+//     const handleChange = (event) => setValue(event.target.value);
+
+//     const handleClick = () => {};
+
+const SearchBarContainer = styled(motion.div)`
+    display: flex;
+    flex-direction: column;
+    width: 34em;
+    height: 3.8em;
+    background-color: #fff;
+    border-radius: 6px;
+    box-shadow: 0px 2px 12px 3px rgba(0, 0, 0, 0.14);
+`;
+
+const SearchInputContainer = styled.div`
+    width: 100%;
+    min-height: 4em;
+    display: flex;
+    align-items: center;
+    position: relative;
+    padding: 2px 15px;
+`;
+
+const SearchInput = styled.input`
+    width: 100%;
+    height: 100%;
+    outline: none;
+    border: none;
+    font-size: 21px;
+    color: #12112e;
+    font-weight: 500;
+    border-radius: 6px;
+    background-color: transparent;
+    &:focus {
+        outline: none;
+        &::placeholder {
+            opacity: 0;
+        }
+    }
+    &::placeholder {
+        color: #bebebe;
+        transition: all 250ms ease-in-out;
+    }
+`;
+
+const SearchIcon = styled.span`
+    color: #bebebe;
+    font-size: 27px;
+    margin-right: 10px;
+    margin-top: 6px;
+    vertical-align: middle;
+`;
+
+const CloseIcon = styled(motion.span)`
+    color: #bebebe;
+    font-size: 23px;
+    vertical-align: middle;
+    transition: all 200ms ease-in-out;
+    cursor: pointer;
+    &:hover {
+        color: #dfdfdf;
+    }
+`;
+
+const LineSeperator = styled.span`
+    display: flex;
+    min-width: 100%;
+    min-height: 2px;
+    background-color: #d8d8d878;
+`;
+
+const SearchContent = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 1em;
+    overflow-y: auto;
+`;
+
+const LoadingWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const WarningMessage = styled.span`
+    color: #a1a1a1;
+    font-size: 14px;
+    display: flex;
+    align-self: center;
+    justify-self: center;
+`;
+
+const containerVariants = {
+    expanded: {
+        height: '30em',
+    },
+    collapsed: {
+        height: '3.8em',
+    },
+};
+
+const containerTransition = { type: 'spring', damping: 22, stiffness: 150 };
 
 const StockSearchBar = () => {
-    const boxColor = useColorModeValue('brand.100', 'brand.700');
-    const [value, setValue] = useState('');
-    const color = useColorModeValue('blue', 'gray');
+    return <div></div>;
 
-    const handleChange = (event) => setValue(event.target.value);
-
-    const handleClick = () => {};
-
-    return (
-        <Box
-            mx={3}
-            mt={5}
-            px={4}
-            py={10}
-            borderRadius='lg'
-            shadow='md'
-            bg={boxColor}
-        >
-            <HStack>
-                <Container centerContent maxW='container.lg'>
-                    <InputGroup>
-                        <InputLeftElement pointerEvents='none' />
-                        <Input
-                            value={value}
-                            onChange={handleChange}
-                            placeholder='Search for symbol'
-                        />
-                        <InputRightElement width='5rem'>
-                            <Button
-                                h='1.75rem'
-                                size='lg'
-                                variant='solid'
-                                colorScheme={color}
-                                onClick={handleClick}
-                            >
-                                {<GoSearch color='white' />}
-                            </Button>
-                        </InputRightElement>
-                    </InputGroup>
-                </Container>
-            </HStack>
-        </Box>
-    );
+    // return (
+    //     <Box
+    //         mx={3}
+    //         mt={5}
+    //         px={4}
+    //         py={10}
+    //         borderRadius='lg'
+    //         shadow='md'
+    //         bg={boxColor}
+    //     >
+    //         <HStack>
+    //             <Container centerContent maxW='container.lg'>
+    //                 <InputGroup>
+    //                     <InputLeftElement pointerEvents='none' />
+    //                     <Input
+    //                         value={value}
+    //                         onChange={handleChange}
+    //                         placeholder='Search for symbol'
+    //                     />
+    //                     <InputRightElement width='5rem'>
+    //                         <Button
+    //                             h='1.75rem'
+    //                             size='lg'
+    //                             variant='solid'
+    //                             colorScheme={color}
+    //                             onClick={handleClick}
+    //                         >
+    //                             {<GoSearch color='white' />}
+    //                         </Button>
+    //                     </InputRightElement>
+    //                 </InputGroup>
+    //             </Container>
+    //         </HStack>
+    //     </Box>
+    // );
 };
 
 export default StockSearchBar;
