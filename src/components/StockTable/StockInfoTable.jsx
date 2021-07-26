4ff4c0d10c8f21515d5bbae01a9c78d5a29d112b
Himanshu Goyal <hgoyal@dhcp-206-87-194-252.ubcsecure.wireless.ubc.ca>
2021-07-25 18:06:14 -0700

    Himanshu close button on hover

--- src/components/StockTable/StockInfoTable.jsx
+++ src/components/StockTable/StockInfoTable.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link, Table, Tbody, Td, Th, Thead, Tr, useColorModeValue} from "@chakra-ui/react";
+import {Box, Link, Table, Tbody, Td, Th, Thead, Tr, useColorModeValue} from "@chakra-ui/react";
 import {Link as ReactRouterLink, useHistory} from "react-router-dom";
 import WatchlistButton from "../WatchlistButton";
 
@@ -26,69 +26,69 @@ const ClickableTd = ({ticker_id, children, ...otherProps}) => {
 
 
 const StockInfoTable = ({stocks}) => (
-    <Table variant="striped"
-           border='4px'
-           borderColor={useColorModeValue('brand.200', 'brand.600')}
-           colorScheme='brand'>
-        <Thead>
-            <Tr>
-                <th/>
-                <Th>Ticker Id</Th>
-                <Th>Dividend Payout Ratio</Th>
-                <Th display={{base: 'none', md: 'table-cell'}}>PE Ratio</Th>
-                <Th display={{base: 'none', md: 'table-cell'}}>PEG Ratio</Th>
-                <Th display={{base: 'none', lg: 'table-cell'}}>Quarterly Earning Growth</Th>
-                <Th display={{base: 'none', lg: 'table-cell'}}>Quarterly Revenue Growth</Th>
-                <Th>52-week Low</Th>
-                <Th>52-week High</Th>
-            </Tr>
-        </Thead>
-        <Tbody>
-            {stocks.map(({
-                             ticker_id,
-                             dividend_payout_ratio,
-                             pe_ratio,
-                             peg_ratio,
-                             quarterly_earning_growth,
-                             quarterly_revenue_growth,
-                             fifty_two_week_low,
-                             fifty_two_week_high
-                         }) =>
-                <Tr
-                    key={ticker_id}
-                    _hover={{cursor: 'pointer'}}
-                >
-                    <Td>
-                        {<WatchlistButton ticker={ticker_id}/>}
-                    </Td>
-                    <ClickableTd isNumeric='false' ticker_id={ticker_id}>
-                        {<StockSymbol ticker={ticker_id}/>}
-                    </ClickableTd>
-                    <ClickableTd ticker_id={ticker_id}>
-                        {dividend_payout_ratio ?? "-"}
-                    </ClickableTd>
-                    <ClickableTd display={{base: 'none', md: 'table-cell'}} ticker_id={ticker_id}>
-                        {pe_ratio ?? "-"}
-                    </ClickableTd>
-                    <ClickableTd display={{base: 'none', md: 'table-cell'}} ticker_id={ticker_id}>
-                        {peg_ratio ?? "-"}
-                    </ClickableTd>
-                    <ClickableTd ticker_id={ticker_id} display={{base: 'none', lg: 'table-cell'}}>
-                        {quarterly_earning_growth ?? "-"}
-                    </ClickableTd>
-                    <ClickableTd ticker_id={ticker_id} display={{base: 'none', lg: 'table-cell'}}>
-                        {quarterly_revenue_growth ?? "-"}
-                    </ClickableTd>
-                    <ClickableTd ticker_id={ticker_id}>
-                        {fifty_two_week_low ?? "-"}
-                    </ClickableTd>
-                    <ClickableTd ticker_id={ticker_id}>
-                        {fifty_two_week_high ?? "-"}
-                    </ClickableTd>
+    <Box borderRadius="lg" border='2px' borderColor={useColorModeValue('brand.200', 'brand.600')}>
+        <Table variant="striped"
+               colorScheme='brand'>
+            <Thead>
+                <Tr>
+                    <th/>
+                    <Th>Ticker Id</Th>
+                    <Th>Dividend Payout Ratio</Th>
+                    <Th display={{base: 'none', md: 'table-cell'}}>PE Ratio</Th>
+                    <Th display={{base: 'none', md: 'table-cell'}}>PEG Ratio</Th>
+                    <Th display={{base: 'none', lg: 'table-cell'}}>Quarterly Earning Growth</Th>
+                    <Th display={{base: 'none', lg: 'table-cell'}}>Quarterly Revenue Growth</Th>
+                    <Th>52-week Low</Th>
+                    <Th>52-week High</Th>
                 </Tr>
-            )}
-        </Tbody>
-    </Table>
+            </Thead>
+            <Tbody>
+                {stocks.map(({
+                                 ticker_id,
+                                 dividend_payout_ratio,
+                                 pe_ratio,
+                                 peg_ratio,
+                                 quarterly_earning_growth,
+                                 quarterly_revenue_growth,
+                                 fifty_two_week_low,
+                                 fifty_two_week_high
+                             }) =>
+                    <Tr
+                        key={ticker_id}
+                        _hover={{cursor: 'pointer'}}
+                    >
+                        <Td>
+                            {<WatchlistButton ticker={ticker_id}/>}
+                        </Td>
+                        <ClickableTd isNumeric='false' ticker_id={ticker_id}>
+                            {<StockSymbol ticker={ticker_id}/>}
+                        </ClickableTd>
+                        <ClickableTd ticker_id={ticker_id}>
+                            {dividend_payout_ratio ?? "-"}
+                        </ClickableTd>
+                        <ClickableTd display={{base: 'none', md: 'table-cell'}} ticker_id={ticker_id}>
+                            {pe_ratio ?? "-"}
+                        </ClickableTd>
+                        <ClickableTd display={{base: 'none', md: 'table-cell'}} ticker_id={ticker_id}>
+                            {peg_ratio ?? "-"}
+                        </ClickableTd>
+                        <ClickableTd ticker_id={ticker_id} display={{base: 'none', lg: 'table-cell'}}>
+                            {quarterly_earning_growth ?? "-"}
+                        </ClickableTd>
+                        <ClickableTd ticker_id={ticker_id} display={{base: 'none', lg: 'table-cell'}}>
+                            {quarterly_revenue_growth ?? "-"}
+                        </ClickableTd>
+                        <ClickableTd ticker_id={ticker_id}>
+                            {fifty_two_week_low ?? "-"}
+                        </ClickableTd>
+                        <ClickableTd ticker_id={ticker_id}>
+                            {fifty_two_week_high ?? "-"}
+                        </ClickableTd>
+                    </Tr>
+                )}
+            </Tbody>
+        </Table>
+    </Box>
 )
 
 
