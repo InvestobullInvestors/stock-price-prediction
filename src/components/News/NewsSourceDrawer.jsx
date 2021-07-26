e9dec7194a08d9ec5f651796e19081da8e5265f8
Paul <51832617+paulsgh@users.noreply.github.com>
2021-07-26 14:36:59 -0700

    update sources btn to filter with icon

--- src/components/News/NewsSourceDrawer.jsx
+++ src/components/News/NewsSourceDrawer.jsx
@@ -1,26 +1,41 @@
 import React from "react";
-import {Button, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, useDisclosure,} from "@chakra-ui/react";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { BiFilterAlt } from "react-icons/bi";
 import NewsSourceChecklist from "./NewsSourceChecklist";
 
 const NewsSourceDrawer = () => {
-    const {isOpen, onOpen, onClose} = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-    return (
-        <>
-            <Button colorScheme="blue" onClick={onOpen} mt={7}>
-                Sources
-            </Button>
-            <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
-                <DrawerOverlay/>
-                <DrawerContent>
-                    <DrawerHeader borderBottomWidth="1px">Sources</DrawerHeader>
-                    <DrawerBody>
-                        <NewsSourceChecklist/>
-                    </DrawerBody>
-                </DrawerContent>
-            </Drawer>
-        </>
-    );
+  return (
+    <>
+      <Button
+        colorScheme="blue"
+        onClick={onOpen}
+        mt={7}
+        ml={3}
+        rightIcon={<BiFilterAlt />}
+      >
+        Filter
+      </Button>
+      <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth="1px">Sources</DrawerHeader>
+          <DrawerBody>
+            <NewsSourceChecklist />
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
 };
 
 export default NewsSourceDrawer;
