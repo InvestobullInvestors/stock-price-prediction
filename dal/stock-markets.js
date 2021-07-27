f8a5502cec45f6e9da1e29351477f1bac79fb123
Himanshu Goyal <hgoyal@dhcp-206-87-194-252.ubcsecure.wireless.ubc.ca>
2021-07-26 21:58:29 -0700

    remove key

--- dal/stock-markets.js
+++ dal/stock-markets.js
@@ -1,15 +1,9 @@
 const mongoose = require("mongoose");
 
-// mongoose.connect(process.env.STOCK_MARKET_MONGO_DB_CONNECTION_STRING, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
-
-mongoose.connect('mongodb+srv://investobull:investobull9876!@stock-price-prediction.bg5zs.mongodb.net/StockMarket', {
+mongoose.connect(process.env.STOCK_MARKET_MONGO_DB_CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-
 
 const Schema = mongoose.Schema;
 
