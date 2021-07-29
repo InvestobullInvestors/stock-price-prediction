433dbf045bd6e937e4b19441d7d4b54235fc534b
Paul <51832617+paulsgh@users.noreply.github.com>
2021-07-29 00:43:28 -0700

    create new verge logos, create button for News page

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -4,8 +4,9 @@ import NewsCardList from "../components/News/NewsCardList";
 import ChecklistDrawer from "../components/News/ChecklistDrawer";
 import CustomHeading from "../components/CustomHeading";
 import StaticChecklistContainer from "../components/News/StaticChecklistContainer";
-import { Grid, useColorModeValue } from "@chakra-ui/react";
+import { Button, Grid, useColorModeValue, VStack } from "@chakra-ui/react";
 import { useStockNews } from "../contexts/useStockNews";
+import { BiFilterAlt } from "react-icons/bi";
 
 const News = () => {
   const {
@@ -21,7 +22,18 @@ const News = () => {
   //TODO: enable watchlist filter
   return (
     <PageTemplate bgColor={useColorModeValue("brand.300", "brand.800")}>
-      <CustomHeading>News</CustomHeading>
+      <VStack>
+        <CustomHeading mb={3}>News</CustomHeading>
+        <Button
+          colorScheme="blue"
+          // onClick={onOpen}
+          rightIcon={<BiFilterAlt />}
+          minW={20}
+          display={["flex", "flex", "none", "none"]}
+        >
+          Filter
+        </Button>
+      </VStack>
       {/*<Button mt={7} bg={color} onClick={handleChangeColor}>watchlist only</Button>*/}
       <Grid display={["none", "none", "flex", "flex"]}>
         <StaticChecklistContainer />
