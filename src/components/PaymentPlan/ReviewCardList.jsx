d2fe99506ae30f16e0ed0aa46009c26d7564c56f
Raghav Thakur <raghav.thakur.rt.20@gmail.com>
2021-08-02 15:12:18 -0700

    Add Raghav and Himanshu profile pictures

--- src/components/PaymentPlan/ReviewCardList.jsx
+++ src/components/PaymentPlan/ReviewCardList.jsx
@@ -6,7 +6,7 @@ const ReviewCardList = () => (
     <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4 }} spacing={3}>
         <ReviewCard
             name="Himanshu Goyal"
-            imageUrl=""
+            imageUrl="https://media-exp1.licdn.com/dms/image/C5603AQGSQ0CWJrsGbg/profile-displayphoto-shrink_800_800/0/1553838277450?e=1633564800&v=beta&t=Ai27ezxA-caXbBRnxPLPH7KWuSoJn3I3kBmNvcHu3oU"
             rating={5}
             reviewTitle="This is the best"
             reviewSummary="I do not know what to write"
@@ -20,7 +20,7 @@ const ReviewCardList = () => (
         />
         <ReviewCard
             name="Raghav"
-            imageUrl=""
+            imageUrl="https://media-exp1.licdn.com/dms/image/C5603AQGBGjphCYLyHg/profile-displayphoto-shrink_200_200/0/1600194405585?e=1633564800&v=beta&t=fd3h-mONGi3IvsaiflQ4q5H4RVebVS57GXbRfOHcLOY"
             rating={5}
             reviewTitle="Amazing job"
             reviewSummary="I like how interactive the website is. It has a nice UI."
