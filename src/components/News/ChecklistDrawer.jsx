726f850cd555e85c18931e9eb5aff8a18edeecd8
Paul <51832617+paulsgh@users.noreply.github.com>
2021-08-07 00:51:07 -0700

    allow user to select sources vs. watchlist from checklist drawer too

--- src/components/News/ChecklistDrawer.jsx
+++ src/components/News/ChecklistDrawer.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import {
     Button,
+    Divider,
     Drawer,
     DrawerBody,
     DrawerContent,
     DrawerHeader,
     DrawerOverlay,
+    Radio,
+    RadioGroup,
+    Stack,
     useDisclosure,
 } from '@chakra-ui/react';
 import { BiFilterAlt } from 'react-icons/bi';
 import NewsChecklist from './NewsChecklist';
+import { useStockNews } from '../../contexts/useStockNews';
+import StockNewsChecklist from './StockNewsChecklist';
 
 const ChecklistDrawer = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const [value, setValue] = React.useState('1');
+    const {
+        isDisplayingWatchlistStockNews,
+        setDisplayWatchlistNews,
+    } = useStockNews();
 
     return (
         <>
@@ -30,9 +41,35 @@ const ChecklistDrawer = () => {
             <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
                 <DrawerOverlay />
                 <DrawerContent>
-                    <DrawerHeader borderBottomWidth="1px">Sources</DrawerHeader>
+                    <DrawerHeader>
+                        <RadioGroup onChange={setValue} value={value}>
+                            <Stack direction="row">
+                                <Radio
+                                    value="1"
+                                    onChange={() =>
+                                        setDisplayWatchlistNews(false)
+                                    }
+                                >
+                                    Sources
+                                </Radio>
+                                <Radio
+                                    value="2"
+                                    onChange={() =>
+                                        setDisplayWatchlistNews(true)
+                                    }
+                                >
+                                    Watchlist Stocks
+                                </Radio>
+                            </Stack>
+                        </RadioGroup>
+                    </DrawerHeader>
+                    <Divider orientation="horizontal" mb={3} />
                     <DrawerBody>
-                        <NewsChecklist />
+                        {isDisplayingWatchlistStockNews ? (
+                            <StockNewsChecklist />
+                        ) : (
+                            <NewsChecklist />
+                        )}
                     </DrawerBody>
                 </DrawerContent>
             </Drawer>
