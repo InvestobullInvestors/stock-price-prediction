4fe45a8857a13c4404ea5fce2c5c4a2b79bcca31
Weiyoung Tan <wtan@bgcengineering.ca>
2021-08-07 21:00:12 -0700

    new styles for menu items

--- src/components/NotificationList.jsx
+++ src/components/NotificationList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { MenuItem, Text } from '@chakra-ui/react';
-import { SmallCloseIcon, Icon } from '@chakra-ui/icons';
+import { Button, MenuItem, Text } from '@chakra-ui/react';
+import { SmallCloseIcon } from '@chakra-ui/icons';
 import { useUser } from '../contexts/useUser';
 
 const NotificationList = () => {
@@ -10,12 +10,16 @@ const NotificationList = () => {
         return <Text m={3}>No new notifications!</Text>;
 
     return notifications.map((item, id) => (
-        <MenuItem
-            key={id}
-            onClick={() => deleteOneNotification(item.timestamp)}
-        >
+        <MenuItem key={id} _hover={{ cursor: 'default' }}>
             <Text mx={2}>{item.text}</Text>
-            <Icon as={SmallCloseIcon} />
+            <Button
+                as={SmallCloseIcon}
+                size="2xs"
+                color="white"
+                bgColor="red.400"
+                _hover={{ cursor: 'pointer' }}
+                onClick={() => deleteOneNotification(item.timestamp)}
+            />
         </MenuItem>
     ));
 };
