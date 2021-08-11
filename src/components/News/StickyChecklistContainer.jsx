6e458e55ee95870ef1b819b0130b712804773eff
Paul <51832617+paulsgh@users.noreply.github.com>
2021-08-10 17:06:41 -0700

    update stickyChecklist UI

--- src/components/News/StickyChecklistContainer.jsx
+++ src/components/News/StickyChecklistContainer.jsx
@@ -1,4 +1,4 @@
-import { Divider, useColorModeValue, VStack } from '@chakra-ui/react';
+import { Divider, VStack } from '@chakra-ui/react';
 import NewsChecklist from './NewsChecklist';
 import React from 'react';
 import CustomBox from '../CustomBox';
@@ -7,20 +7,10 @@ import { useStockNews } from '../../contexts/useStockNews';
 import CustomButtonGroup from '../CustomButtonGroup';
 
 const StickyChecklistContainer = () => {
-    const boxColor = useColorModeValue('brand.400', 'brand.700');
-
     const { isDisplayingWatchlistStockNews } = useStockNews();
 
     return (
-        <CustomBox
-            p="4"
-            mt={5}
-            ml={-1}
-            bgColor={boxColor}
-            position="fixed"
-            boxShadow="dark-lg"
-            zIndex={100}
-        >
+        <CustomBox p="4" mt={5} ml={-1} position="fixed" zIndex={100}>
             <VStack>
                 <CustomButtonGroup />
                 <Divider orientation="horizontal" />
