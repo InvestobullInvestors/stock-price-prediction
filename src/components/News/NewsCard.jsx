fb619785791293ec595f62ced54f9ad13774a9ef
Paul <51832617+paulsgh@users.noreply.github.com>
2021-08-10 21:44:01 -0700

    add scroll down icon for newscards

--- src/components/News/NewsCard.jsx
+++ src/components/News/NewsCard.jsx
@@ -8,10 +8,14 @@ import {
     Flex,
     Spacer,
     HStack,
+    PopoverTrigger,
+    Popover,
+    PopoverContent,
 } from '@chakra-ui/react';
 import { useStockNews } from '../../contexts/useStockNews';
 import NewsArticle from './NewsArticle';
 import CustomBox from '../CustomBox';
+import { BiChevronsDown } from 'react-icons/bi';
 
 const NewsCard = ({ source, children, ...otherProps }) => {
     const { newsInfo } = useStockNews();
@@ -56,104 +60,148 @@ const NewsCard = ({ source, children, ...otherProps }) => {
         >
             {children}
             <Flex display={['none', 'none', 'flex', 'flex']} align="flex-start">
-                <HStack ml="20px" mr="30px" mt="-15px" w="full">
-                    <Square
-                        w={['20%', '20%', '15%', '15%']}
-                        h="150px"
-                        borderRadius="lg"
-                        p="10px"
-                    >
-                        <Image src={logo} />
-                    </Square>
-                    <Spacer />
-                    <CustomBox
-                        w={['78%', '78%', '83%', '83%']}
-                        h="215px"
+                <Popover trigger="hover">
+                    <PopoverTrigger>
+                        <HStack ml="20px" mr="30px" mt="-15px" w="full">
+                            <Square
+                                w={['20%', '20%', '15%', '15%']}
+                                h="150px"
+                                borderRadius="lg"
+                                p="10px"
+                            >
+                                <Image src={logo} />
+                            </Square>
+                            <Spacer />
+                            <CustomBox
+                                w={['78%', '78%', '83%', '83%']}
+                                h="215px"
+                                border={0}
+                                shadow="none"
+                                m={0}
+                                p={2}
+                                overflow="auto"
+                                css={{
+                                    scrollbarWidth: 'none',
+                                    msOverflowStyle: 'none',
+                                    '&::-webkit-scrollbar': {
+                                        width: '0px',
+                                    },
+                                }}
+                            >
+                                <VStack align="flex-start">
+                                    {articles.map((article) => (
+                                        <CustomBox
+                                            key={article._id}
+                                            w="100%"
+                                            border={0}
+                                            shadow="none"
+                                            m={0}
+                                            p={0}
+                                        >
+                                            <NewsArticle
+                                                date={article.publishedAt}
+                                                title={article.title}
+                                                url={article.url}
+                                            />
+                                            <Divider
+                                                my={2}
+                                                orientation="horizontal"
+                                            />
+                                        </CustomBox>
+                                    ))}
+                                </VStack>
+                            </CustomBox>
+                        </HStack>
+                    </PopoverTrigger>
+                    <PopoverContent
+                        background="transparent"
                         border={0}
-                        shadow="none"
-                        m={0}
-                        p={2}
-                        overflow="auto"
-                        css={{
-                            scrollbarWidth: 'none',
-                            msOverflowStyle: 'none',
-                            '&::-webkit-scrollbar': {
-                                width: '0px',
-                            },
-                        }}
+                        shadow={0}
+                        mt="-25px"
                     >
-                        <VStack align="flex-start">
-                            {articles.map((article) => (
-                                <CustomBox
-                                    key={article._id}
-                                    w="100%"
-                                    border={0}
-                                    shadow="none"
-                                    m={0}
-                                    p={0}
-                                >
-                                    <NewsArticle
-                                        date={article.publishedAt}
-                                        title={article.title}
-                                        url={article.url}
-                                    />
-                                    <Divider my={2} orientation="horizontal" />
-                                </CustomBox>
-                            ))}
-                        </VStack>
-                    </CustomBox>
-                </HStack>
+                        <Flex justify="center">
+                            <BiChevronsDown
+                                size={40}
+                                opacity={0.7}
+                                display="inline-block"
+                                w="100%"
+                            />
+                        </Flex>
+                    </PopoverContent>
+                </Popover>
             </Flex>
             <Flex display={['flex', 'flex', 'none', 'none']} align="flex-start">
-                <VStack mt="10px" align="center" w="full">
-                    <CustomBox
-                        w="100px"
-                        border={0}
-                        shadow="none"
-                        p="10px"
-                        mt={-9}
-                        mb={-6}
-                    >
-                        <Image src={logo} />
-                    </CustomBox>
-                    <Spacer />
-                    <CustomBox
-                        w="100%"
-                        h="150px"
-                        border={0}
-                        shadow="none"
-                        m={0}
-                        padding="5px"
-                        overflow="auto"
-                        css={{
-                            scrollbarWidth: 'none',
-                            msOverflowStyle: 'none',
-                            '&::-webkit-scrollbar': {
-                                width: '0px',
-                            },
-                        }}
-                    >
-                        <VStack align="flex-start">
-                            {articles.map((article) => (
-                                <CustomBox
-                                    key={article._id}
-                                    w="100%"
-                                    border={0}
-                                    shadow="none"
-                                    m={0}
-                                    p={0}
-                                >
-                                    <NewsArticle
-                                        date={article.publishedAt}
-                                        title={article.title}
-                                        url={article.url}
-                                    />
-                                    <Divider my={2} orientation="horizontal" />
-                                </CustomBox>
-                            ))}
+                <Popover trigger="hover">
+                    <PopoverTrigger>
+                        <VStack mt="10px" align="center" w="full">
+                            <CustomBox
+                                w="100px"
+                                border={0}
+                                shadow="none"
+                                p="10px"
+                                mt={-9}
+                                mb={-6}
+                            >
+                                <Image src={logo} />
+                            </CustomBox>
+                            <Spacer />
+                            <CustomBox
+                                w="100%"
+                                h="150px"
+                                border={0}
+                                shadow="none"
+                                m={0}
+                                padding="5px"
+                                overflow="auto"
+                                css={{
+                                    scrollbarWidth: 'none',
+                                    msOverflowStyle: 'none',
+                                    '&::-webkit-scrollbar': {
+                                        width: '0px',
+                                    },
+                                }}
+                            >
+                                <VStack align="flex-start">
+                                    {articles.map((article) => (
+                                        <CustomBox
+                                            key={article._id}
+                                            w="100%"
+                                            border={0}
+                                            shadow="none"
+                                            m={0}
+                                            p={0}
+                                        >
+                                            <NewsArticle
+                                                date={article.publishedAt}
+                                                title={article.title}
+                                                url={article.url}
+                                            />
+                                            <Divider
+                                                my={2}
+                                                orientation="horizontal"
+                                            />
+                                        </CustomBox>
+                                    ))}
+                                </VStack>
+                            </CustomBox>
                         </VStack>
-                    </CustomBox>
-                </VStack>
+                    </PopoverTrigger>
+                    <PopoverContent
+                        background="transparent"
+                        border={0}
+                        shadow={0}
+                        mt="-25px"
+                    >
+                        <Flex justify="center">
+                            <BiChevronsDown
+                                size={40}
+                                opacity={0.7}
+                                display="inline-block"
+                                w="100%"
+                            />
+                        </Flex>
+                    </PopoverContent>
+                </Popover>
             </Flex>
         </CustomBox>
     );
